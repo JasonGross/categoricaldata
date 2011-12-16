34731c2c1843b74b2f6aca9d841735e73ffb9d3d
David Spivak <dspivak@gmail.com>
2011-12-15 21:59:43 -0500

    Fixed test.scala to the best of my abilities; still not compiling.

--- src/test/scala/net/metaphor/Test.scala
+++ src/test/scala/net/metaphor/Test.scala
@@ -13,89 +13,82 @@ import scala.math._
 
 @RunWith(classOf[JUnitRunner])
 class Test extends FlatSpec with ShouldMatchers {
-	// NOTE to use the DSL, you need this line:
-	import net.metaphor.dsl.Sentences._
+  // NOTE to use the DSL, you need this line:
+  import net.metaphor.dsl.Sentences._
+
+  /* 
+   * GRAPHS!
+   */
+  // NOTE, need commas between lines
   
+ 
+  val Grph = category (
+    objects = List("an edge", "a vertex"),
+    arrows = List(
+      "an edge" --- "has as source" --> "a vertex",
+      "an edge" --- "has as target" --> "a vertex")
+  )
+
   
-//  /* 
-//   * GRAPHS!
-//   */
-//	// NOTE, need commas between lines
-//  val Grph = model(
-//      "an edge"---"has as source"-->"a vertex",
-//      "an edge"---"has as target"-->"a vertex"
-//  )
-// 
-//  val termGraph = dataset (Grph==>Set) (
-//      "an edge" mapsto ("+1"),
-//      "a vertex" mapsto ("N")
-//  )(
-//      "an edge"---"has as source"-->"a vertex" mapsto (
-//    	"+1" -> "N"
-//    		  ),
-//      "an edge"---"has as target"-->"a vertex" mapsto (
-//    	"+1" -> "N"
-//    		  )
-//  )
-//  
-//  val termBigraph = dataset (Grph==>Set) (
-//      "an edge" mapsto ("input","output"),
-//      "a vertex" mapsto ("species","transition")
-//  )(
-//      "an edge"---"has as source"-->"a vertex" mapsto (
-//    	"input" -> "species",
-//    	"output"-> "transition"
-//    		  ),
-//      "an edge"---"has as target"-->"a vertex" mapsto (
-//    	"input" -> "transition",
-//    	"output"-> "species"
-//    		  )
-//  )
-//  
-//  val initgraph = dataset (Grph==>Set) (
-//      "an edge" mapsto (),
-//      "a vertex" mapsto ()
-//  )(
-//      "an edge"---"has as source"-->"a vertex" mapsto (
-//    		  ),
-//      "an edge"---"has as target"-->"a vertex" mapsto (
-//    		  )
-//  )
-//  
-//  val DavidsFunkyGraph = dataset (Grph==>Set) (
-//      "an edge" mapsto ("f","g","h","i","j"),
-//      "a vertex" mapsto ("A","B","C","D")
-//  )(
-//      "an edge"---"has as source"-->"a vertex" mapsto (
-//    	"f" -> "A",
-//    	"g" -> "A",
-//    	"h" -> "B",
-//    	"i" -> "A",
-//    	"j" -> "C",
-//    	"k" -> "C"
-//    		  ),
-//      "an edge"---"has as target"-->"a vertex" mapsto (
-//    	"f" -> "B",
-//    	"g" -> "B",
-//    	"h" -> "C",
-//    	"i" -> "C",
-//    	"j" -> "C",
-//    	"k" -> "C"
-//    		  )
-//  )
-//    
-//  val D = ???
-//  val F = ???
-//  val d = ???
-//  val e = ??? 
-//  
-//  "pushforward" should "work" in {
-////    F._*(d) should equal (e)
-//  }
-//  
-//  "shriek" should "work" in {
-//    1+2 should equal (4)
-//  }
-  		
-  
+        
+  val termGraph = dataset (source = Grph,
+    onObjects = Map(
+        "an edge" -> List("+1"),
+        "a vertex" -> List("N")),
+    onMorphisms = Map(
+        ("an edge" --- "has as source" --> "a vertex") -> Map ("+1" -> "N"),
+        ("an edge" --- "has as target" --> "a vertex") -> Map ("+1" -> "N"))
+  )
+
+  val termBigraph = dataset(source = Grph,
+    onObjects = Map(
+    	"an edge" -> List ("input", "output"),
+    	"a vertex" -> List ("species", "transition")),
+    onMorphisms = Map(
+    	("an edge" --- "has as source" --> "a vertex") -> Map (
+    		"input" -> "species",
+    		"output" -> "transition"),
+    	("an edge" --- "has as target" --> "a vertex") -> Map (
+    		"input" -> "transition",
+    		"output" -> "species"))
+   )
+
+  val initgraph = dataset (source = Grph
+    onObjects = Map (
+        "an edge" -> Nil,
+        "a vertex" -> Nil),
+    onMorphisms = Map(
+    	("an edge" --- "has as source" --> "a vertex") -> Map(),
+    	("an edge" --- "has as target" --> "a vertex") -> Map())
+  )
+
+  val DavidsFunkyGraph = dataset(source = Grph
+    onObjects = Map (
+        "an edge" -> List ("f", "g", "h", "i", "j"),
+        "a vertex" -> List ("A", "B", "C", "D")),
+    onMorphisms = Map ( 
+    	("an edge" --- "has as source" --> "a vertex") -> Map (
+    			"f" -> "A",
+    			"g" -> "A",
+    			"h" -> "B",
+    			"i" -> "A",
+    			"j" -> "C",
+    			"k" -> "C"),
+        ("an edge" --- "has as target" --> "a vertex") -> Map (
+        		"f" -> "B",
+        		"g" -> "B",
+        		"h" -> "C",
+        		"i" -> "C",
+        		"j" -> "C",
+        		"k" -> "C"))
+   )
+
+  "pushforward" should "work" in {
+    //    F._*(d) should equal (e)
+  }
+
+  "shriek" should "work" in {
+    1 + 2 should equal(4)
+  }
+
 }
