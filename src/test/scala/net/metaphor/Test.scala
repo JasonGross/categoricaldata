66fff16c5a3fdec97cfa0720692ab945418af017
David Spivak <dspivak@gmail.com>
2011-12-16 21:31:53 -0500

    Added some ordinals and some functors, till it broke. Breakage occurred because I included a path. Need guidance.

--- src/test/scala/net/metaphor/Test.scala
+++ src/test/scala/net/metaphor/Test.scala
@@ -40,11 +40,11 @@ class Test extends FlatSpec with ShouldMatchers {
         ("an edge" --- "has as target" --> "a vertex") -> Map ("+1" -> "N"))
   )
 
-  val termBigraph = dataset(source = Grph,
-    onObjects = Map(
+  val termBigraph = dataset (source = Grph,
+    onObjects = Map (
     	"an edge" -> List ("input", "output"),
     	"a vertex" -> List ("species", "transition")),
-    onMorphisms = Map(
+    onMorphisms = Map (
     	("an edge" --- "has as source" --> "a vertex") -> Map (
     		"input" -> "species",
     		"output" -> "transition"),
@@ -53,16 +53,16 @@ class Test extends FlatSpec with ShouldMatchers {
     		"output" -> "species"))
    )
 
-  val initgraph = dataset (source = Grph
+  val initGraph = dataset (source = Grph,
     onObjects = Map (
-        "an edge" -> Nil,
-        "a vertex" -> Nil),
+        "an edge" -> List(),
+        "a vertex" -> List()),
     onMorphisms = Map(
     	("an edge" --- "has as source" --> "a vertex") -> Map(),
     	("an edge" --- "has as target" --> "a vertex") -> Map())
   )
 
-  val DavidsFunkyGraph = dataset(source = Grph
+  val DavidsFunkyGraph = dataset(source = Grph,
     onObjects = Map (
         "an edge" -> List ("f", "g", "h", "i", "j"),
         "a vertex" -> List ("A", "B", "C", "D")),
@@ -82,6 +82,65 @@ class Test extends FlatSpec with ShouldMatchers {
         		"j" -> "C",
         		"k" -> "C"))
    )
+   
+  /* 
+   * Ordinals
+   * TO DO: Some kind of schematic for Ord[n].
+   */
+  val Ord0 = category(
+	objects =List ("V0"),
+    arrows = List ()
+  )
+  
+  val Ord1 = category(
+    objects = List ("V0","V1"),
+    arrows =List ("V0"---"E01"-->"V1")
+  )
+  
+  val Ord2 = category(
+    objects = List ("V0","V1","V2"),
+    arrows = List (
+    		"V0"---"E01"-->"V1",
+    		"V1"---"E12"-->"V2")
+  )
+
+  val Ord3 = category(
+    objects = List ("V0","V1","V2","V3"),
+    arrows = List (
+    		  "V0"---"E01"-->"V1",
+    		  "V1"---"E12"-->"V2",
+    		  "V2"---"E23"-->"V3")
+      
+  )
+ 
+  
+  val Domain = functor (
+    source = Ord0,
+    target = Ord1,
+	onObjects = Map ("V0" -> "V0"),
+	onMorphisms = Map ()
+  )
+ 
+  val Codomain = functor (
+    source = Ord0,
+    target = Ord1,
+	onObjects = Map ("V0" -> "V1"),
+	onMorphisms = Map ()
+  )
+	  
+  
+      
+  val Compose = functor (
+  	source = Ord1,
+    target = Ord2,
+	onObjects = Map (
+	    "V0" -> "V0",
+	    "V1" -> "V2"),
+	onMorphisms = Map (
+	    ("V0"---"E01"-->"V1") -> ("V0"---"E01"-->"V1"---"E12"-->"V2"))
+  )
+      	   
+  val terminalCat = Ord0
 
   "pushforward" should "work" in {
     //    F._*(d) should equal (e)
