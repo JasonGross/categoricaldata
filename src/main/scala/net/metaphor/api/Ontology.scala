02a8b9b37a9c84bb297fdf544948cd84fa717fc1
Scott Morrison <scott@tqft.net>
2011-12-18 15:58:27 -0800

    getting Ontology.scala to compile again

--- src/main/scala/net/metaphor/api/Ontology.scala
+++ src/main/scala/net/metaphor/api/Ontology.scala
@@ -42,12 +42,40 @@ trait Ontology extends FinitelyPresentedCategory[Box, Path, Ontology] { ontology
       } else {
         ontology.relations(source, target)
       }}
-    def toString () = {
+    
+    /* TODO (David) toString needs to *return* a string, not print one.
+     * Also, please don't commit code that doesn't compile: at very least, comment out the lines that aren't compiling,
+     * and leave a comment explaining what's wrong. ---S
+     */
+    
+    /*
+     *  Here you're actually "overriding" an existing method (every Scala object comes with a predefined toString method,
+     *  but by default it isn't very useful). Whenever you override, it's good practice to write "override def" instead of just "def".
+     *  This tells the compiler to generate an error if, in fact, you're *not* overriding something.
+     *  
+     *  Moreover, once a method has the "override" keyword, anyone who later overrides it has to explicitly mark their method with "override".
+     *  The default "toString" already has the "override" keyword, so it's actually necessary here.
+     *  
+     *  I've added it for you, but try removing it to see the compile error.
+     */
+    
+    override def toString() = {
       println("Ontology")
       println("Objects: ")
       for (i <- objects) println(i)
       println();println("Arrows: ")
-      for (i <- arrows) println(i)}
+      /*
+       * TODO (David) I commented the next line  out, as it wasn't compiling. You're looking for "allGenerators", rather than arrows. Remind me
+       * to give you a quick lesson on working out what methods are defined on which classes.  ---S
+       */
+//      for (i <- arrows) println(i)
+      
+      /*
+       *  Finally, I'm adding the next line (which just generates an exception, if the program ever gets to here, because
+       *  the toString method must return a String, and the compiler knows this and complains if it can't see a return value being generated.
+       */
+      ???
+      }
     
   }
   
