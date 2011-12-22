d446f9cc963ec9b5816bfce478977f7b9e519228
Scott Morrison <scott@tqft.net>
2011-12-22 17:29:00 -0600

    FunctorTo, etc

--- src/main/scala/net/metaphor/api/Category.scala
+++ src/main/scala/net/metaphor/api/Category.scala
@@ -6,17 +6,24 @@ trait Category[O, M, C <: Category[O, M, C]] { self: C =>
   def target(m: M): O
   def compose(m1: M, m2: M): M
 
-  abstract class FunctorFrom[OT, MT, CT <: Category[OT, MT, CT]] extends HeteroFunctor[O, M, C, OT, MT, CT] {
+  trait FunctorFrom[TO, TM, TC <: Category[TO, TM, TC]] extends HeteroFunctor[O, M, C, TO, TM, TC] {
     override val source = self
   }
-  abstract class NaturalTransformationFrom[OT, MT, CT <: Category[OT, MT, CT], F <: FunctorFrom[OT, MT, CT]] extends HeteroNaturalTransformation[O, M, C, OT, MT, CT, F] {
+  trait NaturalTransformationFrom[TO, TM, TC <: Category[TO, TM, TC], F <: FunctorFrom[TO, TM, TC]] extends HeteroNaturalTransformation[O, M, C, TO, TM, TC, F] {
+    override def source: F
+    override def target: F
+  }
+  trait FunctorTo[SO, SM, SC <: Category[SO, SM, SC]] extends HeteroFunctor[SO, SM, SC, O, M, C] {
+    override val target = self
+  }
+  trait NaturalTransformationTo[SO, SM, SC <: Category[SO, SM, SC], F <: FunctorTo[SO, SM, SC]] extends HeteroNaturalTransformation[SO, SM, SC, O, M, C, F] {
     override def source: F
     override def target: F
   }
 
-  abstract class FunctorToSet extends FunctorFrom[Set, Function, Sets] with net.metaphor.api.FunctorToSet[O, M, C] 
+  trait FunctorToSet extends FunctorFrom[Set, Function, Sets] with net.metaphor.api.FunctorToSet[O, M, C] 
 
-  abstract class NaturalTransformationToSet[F <: FunctorToSet] extends NaturalTransformationFrom[Set, Function, Sets, F] with net.metaphor.api.NaturalTransformationToSet[O, M, C, F] {
+  trait NaturalTransformationToSet[F <: FunctorToSet] extends NaturalTransformationFrom[Set, Function, Sets, F] with net.metaphor.api.NaturalTransformationToSet[O, M, C, F] {
     override def source: F
     override def target: F
   }
