f6a3e1bbccec1ab736edd7b5a212a61add711f77
Scott Morrison <scott@tqft.net>
2011-12-30 10:39:31 -0600

    fiddling with TerminalOntology

--- src/main/scala/net/metaphor/api/TerminalOntology.scala
+++ src/main/scala/net/metaphor/api/TerminalOntology.scala
@@ -1,19 +1,17 @@
 package net.metaphor.api
 
-//trait TerminalFinitelyGeneratedCategory[OO, MM, C <: FinitelyPresentedCategory[C]] extends FinitelyPresentedCategory[C] with TerminalObject[OO, MM] { self: C =>
-//  type O = OO
-//  type M = MM
+//trait TerminalFinitelyGeneratedCategory extends FinitelyPresentedCategory with TerminalObject { 
 //  val minimumLevel = 0
 //  val maximumLevel = 0
 //  def objectsAtLevel(k: Int) = if(k == 0) List(terminalObject) else Nil
-//  override def generators(source: O, target: O) = List(morphismFrom(terminalObject))
+//  override def generators(source: O, target: O) = Nil
 //}
-//trait TerminalFinitelyPresentedCategory[O, M, C <: FinitelyPresentedCategory[C]] extends TerminalFinitelyGeneratedCategory[O, M, C] { self: C =>
+//trait TerminalFinitelyPresentedCategory extends TerminalFinitelyGeneratedCategory { 
 //  override def relations(source: O, target: O) = Nil
 //}
 //
 //// TODO implement InitialOntology similarly
-//object TerminalOntology extends TerminalFinitelyPresentedCategory[Box, Path, Ontology] with Ontology {
+//object TerminalOntology extends TerminalFinitelyPresentedCategory with Ontology {
 //  val terminalObject = Box("*")
 //  def morphismFrom(o: Box) = identity(terminalObject)
 //}
