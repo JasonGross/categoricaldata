18106431fcef2e380c52553cdbedf12e46d6263b
Scott Morrison <scott@tqft.net>
2011-12-31 14:03:37 -0600

    adding some minimal profiling for right pushforward

---
+++ src/test/scala/net/metaphor/api/RightPushforwardProfiler.scala
@@ -0,0 +1,50 @@
+package net.metaphor.api
+import net.metaphor.examples.Examples
+import net.tqft.toolkit.collections.NonStrictNaturalNumbers
+
+object RightPushforwardProfiler extends App {
+  import net.metaphor.dsl.Sentences._
+
+  val DavidsFunkyGraph = Dataset(source = Examples.Grph,
+    onObjects = Map(
+      "an edge" -> List("f", "g", "h", "i", "j"),
+      "a vertex" -> List("A", "B", "C", "D")),
+    onMorphisms = Map(
+      ("an edge" --- "has as source" --> "a vertex") -> Map(
+        "f" -> "A",
+        "g" -> "A",
+        "h" -> "B",
+        "i" -> "A",
+        "j" -> "C"),
+      ("an edge" --- "has as target" --> "a vertex") -> Map(
+        "f" -> "B",
+        "g" -> "B",
+        "h" -> "C",
+        "i" -> "C",
+        "j" -> "C")))
+  val DavidsFunkyGraphReversed = Dataset(source = Examples.Grph,
+    onObjects = Map(
+      "an edge" -> List("f", "g", "h", "i", "j"),
+      "a vertex" -> List("A", "B", "C", "D")),
+    onMorphisms = Map(
+      ("an edge" --- "has as source" --> "a vertex") -> Map(
+        "f" -> "B",
+        "g" -> "B",
+        "h" -> "C",
+        "i" -> "C",
+        "j" -> "C"),
+      ("an edge" --- "has as target" --> "a vertex") -> Map(
+        "f" -> "A",
+        "g" -> "A",
+        "h" -> "B",
+        "i" -> "A",
+        "j" -> "C")))
+
+  for (n <- NonStrictNaturalNumbers) {
+    val LHS = Examples.ReverseGraph.__*(DavidsFunkyGraph)
+    val RHS = DavidsFunkyGraphReversed
+    LHS.isIsomorphicTo(RHS)
+    println(n)
+  }
+
+}
