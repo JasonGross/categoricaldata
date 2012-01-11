915b27a3157bacdd90cedfe1afcdbfb78ff86541
David Spivak <dspivak@gmail.com>
2012-01-11 09:28:00 -0500

    Tried to fix some errors in CoproductProductTest.scala, and i did make things better, but there are still errors -- not sure why. I also imported FSets as Scott showed me, with Cmd-1. Very cool.

--- src/test/scala/net/metaphor/CoproductProductTest.scala
+++ src/test/scala/net/metaphor/CoproductProductTest.scala
@@ -1,5 +1,6 @@
 package net.metaphor
 
+import net.metaphor.api.Box
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 import org.junit.runner.RunWith
@@ -13,6 +14,7 @@ import net.metaphor.api.FiniteMorphisms
 import net.metaphor.api.Ontologies
 import net.metaphor.examples.Examples
 import net.metaphor.util.CustomMatchers
+import net.metaphor.api.FSet
 /*
  * This should always compile when checked in.
  */
@@ -22,7 +24,7 @@ class CoproductProductTest extends FlatSpec with ShouldMatchers with CustomMatch
   // NOTE to use the DSL, you need this line:
   import net.metaphor.dsl.Sentences._
   
-//  "Product of sets" should "correctly multiply 3 by 2" in { //TODO Scala isn't finding FSet. I tried importing Sets.scala and everything it imports, but to no avail.
+//  "Product of sets" should "correctly multiply 3 by 2" in { //TODO Scala isn't finding beIsomorphicTo
 //    val Three: FSet = List ("1","2", "3")
 //    val Two: FSet = List ("a","b")
 //    val LHS = Sets.product(Three,Two)
@@ -30,7 +32,7 @@ class CoproductProductTest extends FlatSpec with ShouldMatchers with CustomMatch
 //    LHS should beIsomorphicTo(RHS)
 //    
 //  }
-  
+//  
 //  "Coproduct of sets" should "correctly add 3 and 2" in 
 //    val Three: FSet = List ("1","2", "3")
 //    val Two: FSet = List ("a","b")
@@ -59,13 +61,13 @@ class CoproductProductTest extends FlatSpec with ShouldMatchers with CustomMatch
         "c" -> "c1")))
 
   
-//  "Product of datasets" should "be taken pointwise on PointedSets" in { //TODO this doesn't compile, probably because my use of onObjects.
+//  "Product of datasets" should "be taken pointwise on PointedSets" in { //TODO this doesn't compile; not sure why.
 //    val PS = Examples.PointedSets
 //    val X=OneTwoThreePointed
 //    val XX = PS.Datasets.product(X,X)
-//    val E =  X.onObjects("an element")
+//    val E =  X(Box("an element"))
 //    val EE = Sets.product(E,E)
-//    val LHS=XX.onObjects("an element")
+//    val LHS=XX(Box("an element"))
 //    val RHS=EE
 //    LHS should beIsomorphicTo(RHS)
 //  }
@@ -74,9 +76,9 @@ class CoproductProductTest extends FlatSpec with ShouldMatchers with CustomMatch
 //    val PS = Examples.PointedSets
 //    val X=OneTwoThreePointed
 //    val XX = PS.Datasets.coproduct(X,X)
-//    val E =  X.onObjects("an element")
+//    val E =  X(Box("an element"))
 //    val EE = Sets.coproduct(E,E)
-//    val LHS=XX.onObjects("an element")
+//    val LHS=XX(Box("an element"))
 //    val RHS=EE
 //    LHS should beIsomorphicTo(RHS)
 //  }
