8154a52e4f17d58624395834edf9048de27efbdd
David Spivak <dspivak@gmail.com>
2012-01-13 08:24:42 -0500

    Made four new dev tests. They don't compile because the necessary value 'IsNaturalIsomorphism' isn't defined. I looked for where I could put it, but wasn't sure.

--- src/test/scala/net/metaphor/NaturalTransformationDevTest.scala
+++ src/test/scala/net/metaphor/NaturalTransformationDevTest.scala
@@ -10,13 +10,41 @@ import net.metaphor.api.FiniteMorphisms
 import net.metaphor.api.Ontologies
 import net.metaphor.examples.Examples
 import net.metaphor.util.CustomMatchers
+import net.metaphor.api.Translation
 /*
  * This should always compile when checked in.
  */
 
 @RunWith(classOf[JUnitRunner])
-class Test extends FlatSpec with ShouldMatchers with CustomMatchers {
+class NaturalTransformationDevTest extends FlatSpec with ShouldMatchers with CustomMatchers {
   // NOTE to use the DSL, you need this line:
   import net.metaphor.dsl.Sentences._
 
+  "leftUnit" should "be an isomorphism for translations that are isomorphisms" in {
+    val C=Examples.Grph
+    val D=Examples.Grph
+    val F=Examples.ReverseGraph
+    IsNaturalIsomorphism(F.leftUnit) should equal(True)
+  }
+  
+  "leftCounit" should "be an isomorphism for translations that are isomorphisms" in {
+    val C=Examples.Grph
+    val D=Examples.Grph
+    val F=Examples.ReverseGraph
+    IsNaturalIsomorphism(F.leftCounit) should equal(True)
+  }
+  
+  "rightUnit" should "be an isomorphism for translations that are isomorphisms" in {
+    val C=Examples.Grph
+    val D=Examples.Grph
+    val F=Examples.ReverseGraph
+    IsNaturalIsomorphism(F.rightUnit) should equal(True)
+  }
+  "rightCounit" should "be an isomorphism for translations that are isomorphisms" in {
+    val C=Examples.Grph
+    val D=Examples.Grph
+    val F=Examples.ReverseGraph
+    IsNaturalIsomorphism(F.rightCounit) should equal(True)
+  }
+  
 }
