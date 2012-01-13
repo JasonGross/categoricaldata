8789e48ed8b7a5ffdb270fb09a483228095a3176
David Spivak <dspivak@gmail.com>
2012-01-13 08:09:34 -0500

    Added NaturalTransformationDevTest.scala

---
+++ src/test/scala/net/metaphor/NaturalTransformationDevTest.scala
@@ -0,0 +1,22 @@
+package net.metaphor
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import scala.math._
+import net.metaphor.api.Ontology
+import net.metaphor.api.FiniteMorphisms
+import net.metaphor.api.Ontologies
+import net.metaphor.examples.Examples
+import net.metaphor.util.CustomMatchers
+/*
+ * This should always compile when checked in.
+ */
+
+@RunWith(classOf[JUnitRunner])
+class Test extends FlatSpec with ShouldMatchers with CustomMatchers {
+  // NOTE to use the DSL, you need this line:
+  import net.metaphor.dsl.Sentences._
+
+}
