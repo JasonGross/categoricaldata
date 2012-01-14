1dedc8ad96872c2099e1cb598db8c537fdf04992
Scott Morrison <scott@tqft.net>
2012-01-14 19:52:51 +0900

    minor

--- src/main/scala/net/metaphor/api/LocallyFinitelyGeneratedCategory.scala
+++ src/main/scala/net/metaphor/api/LocallyFinitelyGeneratedCategory.scala
@@ -13,7 +13,8 @@ case class Path[O, G](source: O, target: O, morphisms: List[G]) {
   // This is purely a micro-optimization.
   override lazy val hashCode = morphisms.hashCode
 
-  override def toString = {
+  // FIXME making this a val for a moment, looking for errors
+  override val toString = {
     val afterFirstQuote = """".*"( --- ".*" --> ".*")""".r
     source.toString + (for (m <- morphisms; s = m.toString) yield afterFirstQuote.unapplySeq(s).get.head).mkString
   }
