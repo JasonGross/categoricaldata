5a402d212ea5d160657a1869f6791991b5cc5339
Scott Morrison <scott@tqft.net>
2012-01-28 23:37:26 -0800

    cleaning up, and writing a bit of the new 'Limits'

--- src/main/scala/net/categoricaldata/category/FunctorsToSet.scala
+++ src/main/scala/net/categoricaldata/category/FunctorsToSet.scala
@@ -5,12 +5,14 @@ import net.categoricaldata.sets._
 trait FunctorToSet extends Functor {
   override val target: Sets.type = Sets  
 }
+
 trait NaturalTransformationToSet extends NaturalTransformation {
   override val source: FunctorToSet
   override val target: FunctorToSet
   
   def isomorphism_? : Boolean = ???
 }
+
 trait FunctorsToSet extends Category {
   type O <: FunctorToSet
   type M <: NaturalTransformationToSet
@@ -21,8 +23,7 @@ trait FunctorsToSet extends Category {
   override def identity(o: O) = internalize(???)
   override def source(m: M) = internalize(m.source)
   override def target(m: M) = internalize(m.target)
-  override def compose(m1: M, m2: M) = internalize(???)
-  
+  override def compose(m1: M, m2: M) = internalize(???)  
 }
 
 object FunctorsToSet extends FunctorsToSet {
--- src/main/scala/net/categoricaldata/universalalgebra/Limits.scala
+++ src/main/scala/net/categoricaldata/universalalgebra/Limits.scala
@@ -1,8 +1,30 @@
 package net.categoricaldata.universalalgebra
 import net.categoricaldata.category._
 
-trait Limits {
+trait Diagrams { category: Category => 
+  trait Diagram {  
+	trait Cone {
+	  def coneObject: O
+	  def coneMap(o: O): M
+	}
+	case class ConeMap(source: Cone, target: Cone, mapBetweenConeObjects: M)
+  
+	trait Cones extends Category {
+	  override type O = Cone
+	  override type M = ConeMap
+	  
+	  override def source(m: ConeMap) = m.source
+	  override def target(m: ConeMap) = m.target
+	  override def identity(o: Cone) = ConeMap(o, o, category.identity(o.coneObject))
+	  override def compose(m1: ConeMap, m2: ConeMap) = ConeMap(m1.source, m2.target, category.compose(m1.mapBetweenConeObjects, m2.mapBetweenConeObjects))
+	}   
+  }
+}
 
+trait Limits extends Diagrams { self: Category =>
+	def limit(d: Diagram): d.Cones with InitialObject
+	def limitCone(d: Diagram) = limit(d).initialObject
+	def limitObject(d: Diagram) = limitCone(d).coneObject
 }
 
 object Limits {
