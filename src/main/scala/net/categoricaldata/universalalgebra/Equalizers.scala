1c92f56d7b6c683782fb1c40dde6867702483e5d
Scott Morrison <scott@tqft.net>
2012-01-29 20:59:49 -0800

    filling in some universal algebra

--- src/main/scala/net/categoricaldata/universalalgebra/Equalizers.scala
+++ src/main/scala/net/categoricaldata/universalalgebra/Equalizers.scala
@@ -22,4 +22,23 @@ trait Equalizers { category: Category =>
   def equalizerUniversality(m: category.M, ms: category.M*): M
 }
 
-// TODO Coequalizers
+trait Coequalizers { category: Category =>
+  class Cocones(ms: category.M*) extends Category {
+    override type O = category.M
+    override type M = (category.M, category.M, category.M)
+
+    override def identity(o: O) = (o, o, category.identity(category.source(o)))
+    override def source(m: M) = m._1
+    override def target(m: M) = m._2
+    override def compose(m1: M, m2: M) = (m1._1, m2._3, category.compose(m1._3, m2._3))
+
+  }
+
+  def coequalizerInitialCocone(ms: category.M*): Cocones with InitialObject = new Cocones(ms: _*) with InitialObject {
+    override def initialObject = coequalizerMorphism(ms: _*)
+    override def morphismFromInitialObject(m: category.M) = (initialObject, m, coequalizerUniversality(m, ms: _*))
+  }
+  def coequalizerMorphism(ms: category.M*): M
+  def coequalizerObject(ms: category.M*): O = category.target(coequalizerMorphism(ms: _*))
+  def coequalizerUniversality(m: category.M, ms: category.M*): M
+}
--- src/main/scala/net/categoricaldata/universalalgebra/Limits.scala
+++ src/main/scala/net/categoricaldata/universalalgebra/Limits.scala
@@ -18,6 +18,22 @@ trait Diagrams { category: Category =>
 	  override def identity(o: Cone) = ConeMap(o, o, category.identity(o.coneObject))
 	  override def compose(m1: ConeMap, m2: ConeMap) = ConeMap(m1.source, m2.target, category.compose(m1.mapBetweenConeObjects, m2.mapBetweenConeObjects))
 	}   
+
+	trait Cocone {
+	  def coconeObject: O
+	  def coconeMap(o: O): M
+	}
+	case class CoconeMap(source: Cocone, target: Cocone, mapBetweenCoconeObjects: M)
+  
+	trait Cocones extends Category {
+	  override type O = Cocone
+	  override type M = CoconeMap
+	  
+	  override def source(m: CoconeMap) = m.source
+	  override def target(m: CoconeMap) = m.target
+	  override def identity(o: Cocone) = CoconeMap(o, o, category.identity(o.coconeObject))
+	  override def compose(m1: CoconeMap, m2: CoconeMap) = CoconeMap(m1.source, m2.target, category.compose(m1.mapBetweenCoconeObjects, m2.mapBetweenCoconeObjects))
+	}   
   }
 }
 
@@ -29,6 +45,18 @@ trait Limits extends Diagrams { self: Category =>
 
 object Limits {
   trait usingProductsAndEqualizers extends Limits { self: Category with Products with Equalizers =>
+    
+  }
+}
+
+trait Colimits extends Diagrams { self: Category =>
+	def colimit(d: Diagram): d.Cocones with TerminalObject
+	def colimitCocone(d: Diagram) = colimit(d).terminalObject
+	def colimitObject(d: Diagram) = colimitCocone(d).coconeObject
+}
+
+object Colimits {
+  trait usingCoproductsAndCoequalizers extends Colimits { self: Category with Coproducts with Coequalizers =>
     
   }
 }
